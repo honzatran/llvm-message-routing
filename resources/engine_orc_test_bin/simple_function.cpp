79d29f72ab3b4f3f28ff43fb51ecf27099c80c99
Tran Tuan Hiep <honza.tran@gmail.com>
2019-07-17 22:45:01 +0200

    - adding resources for the c++ source code

---
+++ resources/engine_orc_test_bin/simple_function.cpp
@@ -0,0 +1,26 @@
+
+
+
+int internal_function(int value)
+{
+    return value;
+}
+
+/// function defined inside the unit test
+extern "C" int external_unit_test_function(int value);
+
+extern "C" int simple_function(int value);
+
+/// function called inside the simple_function unit test
+int simple_function(int value)
+{
+    return internal_function(value);
+}
+
+extern "C" int calling_external_function(int value);
+
+/// function called inside the external_function unit test
+int calling_external_function(int value)
+{
+    return external_unit_test_function(value);
+}
